160997d34e00eeab3990ea04b31cd6be8457238a
awonosuke <blueno091117@gmail.com>
2022-08-22 23:45:16 +0900

    [change] finish section-69

---
+++ handler/list_task.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"github.com/awonosuke/rest-api-sample/entity"
+	"github.com/awonosuke/rest-api-sample/store"
+	"net/http"
+)
+
+type ListTask struct {
+	Store *store.TaskStore
+}
+
+type task struct {
+	ID     entity.TaskID     `json:"id"`
+	Title  string            `json:"title"`
+	Status entity.TaskStatus `json:"status"`
+}
+
+func (lt *ListTask) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	tasks := lt.Store.All()
+	rsp := []task{}
+	for _, t := range tasks {
+		rsp = append(rsp, task{
+			ID:     t.ID,
+			Title:  t.Title,
+			Status: t.Status,
+		})
+	}
+	ResponseJSON(ctx, w, rsp, http.StatusOK)
+}

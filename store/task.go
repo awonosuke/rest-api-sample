ac0280a4551eb12430c12c7e0dfb097edd44391e
awonosuke <blueno091117@gmail.com>
2022-08-29 00:01:38 +0900

    [fix] fix mis-type

--- store/task.go
+++ store/task.go
@@ -9,13 +9,13 @@ func (r *Repository) AddTask(
 	ctx context.Context, db Execer, t *entity.Task,
 ) error {
 	t.Created = r.Clocker.Now()
-	t.Moodified = r.Clocker.Now()
+	t.Modified = r.Clocker.Now()
 	sql := `INSERT INTO task
 		(title, status, created, modified)
 	VALUES (?, ?, ?, ?)`
 	result, err := db.ExecContext(
 		ctx, sql, t.Title, t.Status,
-		t.Created, t.Moodified,
+		t.Created, t.Modified,
 	)
 	if err != nil {
 		return err
